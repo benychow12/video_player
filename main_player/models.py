b68341b66f6c02bf46c3888ff959f777ff995b35
benychow12 <bennychou94@gmail.com>
2020-09-01 20:43:47 -0700

    Clean up

--- main_player/models.py
+++ main_player/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class video_model(models.Model):
+    video_filename = models.CharField(max_length=200)
+    video_file_extension = models.CharField(max_length=200)
+    video_sub_enable = models.BooleanField(default=False)
+    
--- video_player/settings.py
+++ video_player/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Vancouver'
 
 USE_I18N = True
 
